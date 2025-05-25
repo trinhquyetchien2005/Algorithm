999f2190ebd32f79e75541d4702ac2a3534f5860
TrinhQuyetChien <trinhquyetchien2005@gmail.com>
2025-05-25 17:57:43 +0700

    feat(all): create and test

---
+++ Letcode/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array[100];
+
+    for(int i = 0; i<= 100; i++){
+        array[i] = i;
+        cout<<array[i];
+    }
+    return 0;
+}
